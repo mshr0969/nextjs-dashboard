85f96c38ad44855a9f322fecba2f91db6c0fdd09
mshr0969 <mshr60531@gmail.com>
2024-05-02 16:21:06 +0900

    create invoice

---
+++ app/lib/actions.ts
@@ -0,0 +1,34 @@
+"use server";
+
+import { date, z } from "zod";
+import { sql } from "@vercel/postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(["paid", "pending"]),
+  date: z.string(),
+});
+
+const CreateInvoive = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoive(formData: FormData) {
+  const { customerId, amount, status } = CreateInvoive.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split("T")[0];
+
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
